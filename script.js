86619395029695fe342e65ae87b57578b26b3b25
dylan-rodarte <173209237+dylan-rodarte@users.noreply.github.com>
2024-09-16 15:27:41 -0500

    feat(createRow): Add function to make row with specified number of pixels

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const container = document.querySelector(".container");
 
-createGrid(3);
+
+createGrid(16);
 
 function createGrid(dimension){
-    for (let i = 0; i < (dimension * dimension); i++){
-        const pixel = document.createElement("div");
-        pixel.textContent = i;
-        container.appendChild(pixel);
+    for (let i = 0; i < dimension; i++){
+        createRow(dimension);
     }
+}
+
+function createRow(dimension){
+    const row = document.createElement("div");
+    row.className = "row";
+    for (let j = 0; j < dimension; j++){
+        const pixel = document.createElement("div");
+        pixel.className = "pixel";
+        pixel.textContent = j;
+        row.appendChild(pixel);
+    }
+    container.appendChild(row);
 }
